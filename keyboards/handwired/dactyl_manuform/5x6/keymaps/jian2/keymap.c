b833c94ac8a0bf7238b44eb986262cb54acfac52
Luo Jian <jian.luo.cn@gmail.com>
2022-09-23 13:30:07 +0200

    update manuform

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/jian2/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/jian2/keymap.c
@@ -1,24 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "keymap_steno.h"
 
-enum jian_keycodes {
-	EXT_PLV = SAFE_RANGE,
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case EXT_PLV:
-      if (record->event.pressed) {
-        layer_off(2);
-      }
-      return false;
-  }
-  return true;
-}
-
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case LALT_T(KC_H):
+    case LALT_T(KC_I):
+    case LALT_T(KC_L):
     case LALT_T(KC_R):
     case LALT_T(KC_S):
     case LCTL_T(KC_D):
@@ -26,8 +12,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     case LGUI_T(KC_A):
     case RALT_T(KC_D):
     case RALT_T(KC_G):
-    case RALT_T(KC_I):
-    case RALT_T(KC_L):
+    case RALT_T(KC_H):
     case RCTL_T(KC_E):
     case RCTL_T(KC_K):
     case RGUI_T(KC_O):
@@ -46,8 +31,6 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
         case LT(6,KC_SPC):
         case LT(7,KC_TAB):
         case LT(8,KC_ESC):
-        case LT(9,KC_SLSH):
-        case LT(9,KC_Z):
         case LT(10,KC_0):
         case LT(10,KC_ENT):
             return false;
