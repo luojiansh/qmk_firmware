43413b67f030d49756ee9d29883fd944ed998cee
Luo Jian <jian.luo.cn@gmail.com>
2022-09-26 16:50:38 +0200

    update ergodash

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/jian2/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/jian2/config.h
@@ -1,5 +1,8 @@
 /*
+This is the c configuration file for the keymap
+
 Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,7 +17,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 
 #pragma once
 
@@ -42,7 +44,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Enables This makes it easier for fast typists to use dual-function keys */
 // Don't work with home row mods.
+#ifdef PERMISSIVE_HOLD
 #undef PERMISSIVE_HOLD
+#endif
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
--- keyboards/omkbd/ergodash/rev1/keymaps/jian/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/jian/keymap.c
@@ -1,1 +1,42 @@
+#include QMK_KEYBOARD_H
+#include "keymap_steno.h"
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LALT_T(KC_I):
+    case LALT_T(KC_L):
+    case LALT_T(KC_R):
+    case LALT_T(KC_S):
+    case LCTL_T(KC_D):
+    case LCTL_T(KC_S):
+    case LGUI_T(KC_A):
+    case RALT_T(KC_D):
+    case RALT_T(KC_G):
+    case RALT_T(KC_H):
+    case RCTL_T(KC_E):
+    case RCTL_T(KC_K):
+    case RGUI_T(KC_O):
+    case RGUI_T(KC_SCLN):
+      return TAPPING_TERM + 100;
+    default:
+      return TAPPING_TERM;
+  }
+}
+
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(3,KC_ENT):
+        case LT(4,KC_BSPC):
+        case LT(5,KC_DEL):
+        case LT(6,KC_SPC):
+        case LT(7,KC_TAB):
+        case LT(8,KC_ESC):
+        case LT(10,KC_0):
+        case LT(10,KC_ENT):
+            return false;
+        default:
+            return true;
+    }
+}
+
 #include "keymap2.c"
